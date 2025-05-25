44b566bef7e8e7ee5a23b51fac45bd12459a1625
EmranHossen <emranhossen3075@gmail.com>
2025-05-25 21:13:16 +0600

    refector : Add Server side Validation

--- BookShelf/BookShelf.Web/Controllers/CategoryController.cs
+++ BookShelf/BookShelf.Web/Controllers/CategoryController.cs
@@ -19,5 +19,27 @@ namespace BookShelf.Web.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display order cannot exactly match the Name");
+            }
+
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name. ");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
     }
 }
