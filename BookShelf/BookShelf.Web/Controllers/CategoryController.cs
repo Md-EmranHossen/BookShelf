174e116851b93b3c824a9144c340ded3ee366da9
EmranHossen <emranhossen3075@gmail.com>
2025-05-25 16:41:48 +0600

    refector: update Category Controller

--- BookShelf/BookShelf.Web/Controllers/CategoryController.cs
+++ BookShelf/BookShelf.Web/Controllers/CategoryController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookShelf.Web.Data;
+using BookShelf.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookShelf.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
